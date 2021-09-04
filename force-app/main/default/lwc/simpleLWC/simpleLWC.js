915394c53f280f2add2fa72e109825da61c267fa
mainak7 <mainaksalesforce@gmail.com>
2021-09-04 12:25:18 +0530

    InClassExercise

--- force-app/main/default/lwc/simpleLWC/simpleLWC.js
+++ force-app/main/default/lwc/simpleLWC/simpleLWC.js
@@ -1,3 +1,7 @@
 import { LightningElement } from 'lwc';
-
-export default class SimpleLWC extends LightningElement {}
+export default class SimpleLWC extends LightningElement {
+    name = 'Hello'
+    connectedCallback() {
+        console.log('came into connectedCallback')
+    }
+}
